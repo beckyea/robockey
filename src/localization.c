63b113af4090cbc52ed0910679ad094d12eedf5a
Becky <14rabramowitz@castilleja.org>
2016-11-08 17:40:08 -0500

    added matlab

--- src/localization.c
+++ src/localization.c
@@ -20,10 +20,13 @@ double findOrientation();
 void setPosition();
 
 int main() {
+	set4Pts(261, 208, 184, 261, 270, 284, 247, 202);
+	//set4Pts(259, 208, 183, 259, 274, 288, 251, 204);
+
 	//set4Pts(492, 330, 473, 368, 567, 367, 513, 402);
-	set3Pts(473, 368, 567, 367, 513, 402); // CASE C
+	//set3Pts(473, 368, 567, 367, 513, 402); // CASE C
 	//set3Pts(492, 330, 473, 368, 567, 367); // CASE A
-	printf("x: %d, y: %d, theta: %f\n", bot.x, bot.y, orientation*180/3.1415);
+	printf("x: %d, y: %d, theta: %f\n", bot.x, bot.y, orientation);
 }
 
 
@@ -36,7 +39,7 @@ void set4Pts(int X1, int Y1, int X2, int Y2, int X3, int Y3, int X4, int Y4) {
 	d14 = pow((X4 - X1), 2) + pow((Y4 - Y1), 2);
 	d23 = pow((X3 - X2), 2) + pow((Y3 - Y2), 2);
 	d24 = pow((X4 - X2), 2) + pow((Y4 - Y2), 2);
-	d34 = pow((X4 - X2), 2) + pow((Y4 - Y2), 2);
+	d34 = pow((X4 - X3), 2) + pow((Y4 - Y3), 2);
 	maxDist = max (max (max(d12, d13), max(d14, d23)), max(d24, d34));
 	minDist = min (min (min(d12, d13), min(d14, d23)), min(d24, d34));
 
@@ -46,11 +49,11 @@ void set4Pts(int X1, int Y1, int X2, int Y2, int X3, int Y3, int X4, int Y4) {
 			B.x = X1; B.y = Y1; D.x = X2; D.y = Y2;
 		} else { D.x = X1; D.y = Y1; B.x = X2; B.y = Y2; }
 	} else if (maxDist == d13) { 
-		if (minDist == d13 || minDist == d14) {
+		if (minDist == d12 || minDist == d14) {
 			B.x = X1; B.y = Y1; D.x = X3; D.y = Y3;
 		} else { D.x = X1; D.y = Y1; B.x = X3; B.y = Y3; }
 	} else if (maxDist == d14) {
-		if (minDist == d13 || minDist == d14) {
+		if (minDist == d13 || minDist == d12) {
 			B.x = X1; B.y = Y1; D.x = X4; D.y = Y4;
 		} else { D.x = X1; D.y = Y1; B.x = X4; B.y = Y4; }
 	} else if (maxDist == d23) {
@@ -68,6 +71,8 @@ void set4Pts(int X1, int Y1, int X2, int Y2, int X3, int Y3, int X4, int Y4) {
 	} else { 
 		printf("Something is wrong. Largest distance is %ld\n", maxDist);
 	}
+	printf("d12: %ld, d13: %ld, d14: %ld, d23: %ld, d24: %ld, d34: %ld\n", d12, d13, d14, d23, d24, d34);
+	printf("minDist: %ld, maxDist: %ld, Bx: %d, By: %d, Dx: %d, Dy: %d\n", minDist, maxDist, B.x, B.y, D.x, D.y);
 	center.x = (B.x + D.x) / 2;
 	center.y = (B.y + D.y) / 2;
 	orientation = findOrientation(B, D);
