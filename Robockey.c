f556a5513f6ad2471e9dbd3f0233517be843d896
adnanjaf <adnan.jafferjee@gmail.com>
2016-11-09 16:42:29 -0500

    Changed mutliple else if statements to a switch case

--- Robockey.c
+++ Robockey.c
@@ -23,14 +23,57 @@ char buffer[PACKET_LENGTH] = {0,0,0,0,0,0,0,0,0,0};
 #define HALF 0xA6
 #define GAME_OVER 0xA7
 
-void timer1_init(void);
+volatile bool play = false;
 
-void init()
-{ 
-	// Define output pins
-	set(DDRB,MOTOR_EN);
+void timer1_init(void);
+void init(void);
+
+int main(void){
+	init();
+	m_usb_init(); // initialize usb
+	timer1_setup(); // setup timer 1
+
+}
+
+void timer1_setup(void){
+	//Timer 1 config
+	set(DDRB,6); //Set B6 to output
+	set(TCCR1B,WGM13); //Set to mode 15 (up to OCR1A)
+	set(TCCR1B,WGM12); // ^
+	set(TCCR1A,WGM11); // ^
+	set(TCCR1A,WGM10); // ^
+	set(TCCR1A,COM1B1); //Set at OCR1B, clear at rollover
+	set(TCCR1A,COM1B0); // ^
+	set(TIMSK1,OCIE1A); //Enable interrupt when TCNT1 = OCR1A
+	set(TIMSK1,OCIE1B); //Enable interrupt when TCNT1 = OCR1B
+	OCR1A = 20000; // Default frequency: 800Hz
+	OCR1B = 18000; // ^ duty cycle: 10%
+}
+
+void init(void){ 
+	set(DDRB,MOTOR_EN); // Define output pins for Motor Control
 	set(DDRB,LMOTOR_D);
 	set(DDRB,RMOTOR_D);
+	
+	m_rf_open(CHANNEL, RXADDRESS, PACKET_LENGTH); // Setup wireless module
+	sei();//enable global interrupts
+	int b = m_wii_open(); // initialize m_wii
+}
 
+
+ISR(INT2_vect) {
+	m_rf_read(buffer, PACKET_LENGTH);	
+	if (buffer[0] == PLAY)
+		play = true;
+	else if (buffer[0] == PAUSE)
+		stop();
+}
+
+ISR(TIMER1_COMPA_vect){    //PWM signal goes low 
+	clear(MOTOR,MOTOR_EN);
+}
+
+ISR(TIMER1_COMPB_vect){   //PWM signal goes high
+	set(MOTOR,MOTOR_EN);
 }
 
