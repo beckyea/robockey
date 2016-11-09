554e6d0dab204c6adbf50f840feaeadeb93aa526
adnanjaf <adnan.jafferjee@gmail.com>
2016-11-09 14:34:47 -0500

    8 ADC Channels Enabled

--- ADC-Puck Finding.c
+++ ADC-Puck Finding.c
@@ -11,7 +11,7 @@
 #include "m_usb.h"
 
 volatile int ADC_flag = 0;
-int ADC_Channels[2] = {0,0}; //Array to hold phototransistor ADC values
+int ADC_Channels[8] = {0,0,0,0,0,0,0,0}; //Array to hold phototransistor ADC values
 int ADC_Check=0; //Int to iterate through phototransistor channels
 
 //Prototyping subroutines 
@@ -56,12 +56,12 @@ int main(void) {
 	//Disable digital on ADC Pins
 	set(DIDR0,ADC0D); // F0
 	set(DIDR0,ADC1D); // F1
-	// set(DIDR0,ADC4D); // F4
-	// set(DIDR0,ADC5D); // F5
-	// set(DIDR0,ADC6D); // F6
-	// set(DIDR0,ADC7D); // F7
-	// set(DIDR2,ADC8D); // D4
-	// set(DIDR2,ADC9D); // D6
+	set(DIDR0,ADC4D); // F4
+	set(DIDR0,ADC5D); // F5
+	set(DIDR0,ADC6D); // F6
+	set(DIDR0,ADC7D); // F7
+	set(DIDR2,ADC8D); // D4
+	set(DIDR2,ADC9D); // D6
 	set(ADCSRA,ADIE); //Enable ADC Interrupt Flag
 	clear(ADCSRA,ADATE); // Disable free-running Mode
 }
@@ -69,9 +69,9 @@ int main(void) {
 ISR(ADC_vect){ //Call Interrupt when conversion completes
 	clear(ADCSRA,ADEN);
 	ADC_Channels[ADC_Check]=ADC;
-	if(ADC_Check==1){   //If all channels read
-		ADC_Check == 0;
-		ADC_Flag == 1;
+	if(ADC_Check==7){   //If all channels read
+		ADC_Check == 0; 
+		ADC_Flag == 1; 
 	}
 	else ADC_Check ++;
 	if(ADC_Check == 0){ // Set ADC to F0
@@ -82,6 +82,42 @@ ISR(ADC_vect){ //Call Interrupt when conversion completes
 	}
 	else if(ADC_Check == 1){ // Set ADC to F1
 		clear(ADMUX,MUX5);
+		clear(ADMUX,MUX2);
+		clear(ADMUX,MUX1);
+		set(ADMUX,MUX0);
+	}
+	else if(ADC_Check == 2){ // Set ADC to F4
+		clear(ADMUX,MUX5);
+		set(ADMUX,MUX2);
+		clear(ADMUX,MUX1);
+		clear(ADMUX,MUX0);
+	}
+	else if(ADC_Check == 3){ // Set ADC to F5
+		clear(ADMUX,MUX5);
+		set(ADMUX,MUX2);
+		clear(ADMUX,MUX1);
+		set(ADMUX,MUX0);
+	}
+	else if(ADC_Check == 4){ // Set ADC to F6
+		clear(ADMUX,MUX5);
+		set(ADMUX,MUX2);
+		set(ADMUX,MUX1);
+		clear(ADMUX,MUX0);
+	}
+	else if(ADC_Check == 5){ // Set ADC to F7
+		clear(ADMUX,MUX5);
+		set(ADMUX,MUX2);
+		set(ADMUX,MUX1);
+		set(ADMUX,MUX0);
+	}
+	else if(ADC_Check == 6){ // Set ADC to D4
+		set(ADMUX,MUX5);
+		clear(ADMUX,MUX2);
+		clear(ADMUX,MUX1);
+		clear(ADMUX,MUX0);
+	}
+	else if(ADC_Check == 7){ // Set ADC to D6
+		set(ADMUX,MUX5);
 		clear(ADMUX,MUX2);
 		clear(ADMUX,MUX1);
 		set(ADMUX,MUX0);
