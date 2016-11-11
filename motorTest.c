3d2de65d82baf8c48a1230ec0a3789f6900fcdf9
adnanjaf <adnan.jafferjee@gmail.com>
2016-11-10 21:59:40 -0500

    Updated motorTest code for testBot

--- motorTest.c
+++ motorTest.c
@@ -1,7 +1,6 @@
 #include "m_general.h"
 #include "m_usb.h"
 #include "m_bus.h"
-#include "m_wii.h"
 #include "m_rf.h"
 
 #define RIGHT 0
@@ -24,39 +23,45 @@ void stop(void);
 int main(void){
 	init();
 	m_usb_init(); // initialize usb
-	timer1_setup(); // setup timer 1
+	// timer1_init(); // setup timer 1
 
 	while(1){
-       fwd(); //Always drive forward (for debugging)
+		set(MOTOR,MOTOR_EN);
+		fwd();
 	}
 
 }
 
-void timer1_setup(void){
+void timer1_init(void){
 	//Timer 1 config
 	set(DDRB,6); //Set B6 to output
 	set(TCCR1B,WGM13); //Set to mode 15 (up to OCR1A)
 	set(TCCR1B,WGM12); // ^
 	set(TCCR1A,WGM11); // ^
 	set(TCCR1A,WGM10); // ^
+	set(TCCR1B,CS12); // set timer prescaler to 1024
+	clear(TCCR1B,CS11);
+	set(TCCR1B,CS10);
 	set(TCCR1A,COM1B1); //Set at OCR1B, clear at rollover
 	set(TCCR1A,COM1B0); // ^
 	set(TIMSK1,OCIE1A); //Enable interrupt when TCNT1 = OCR1A
 	set(TIMSK1,OCIE1B); //Enable interrupt when TCNT1 = OCR1B
-	OCR1A = 20000; // Default frequency: 800Hz
-	OCR1B = 18000; // ^ duty cycle: 10%
+	OCR1A = 10000; // Default frequency: 800Hz
+	OCR1B = 5000; // ^ duty cycle: 50%
 }
 
 void init(void){ 
 	set(DDRB,MOTOR_EN); // Define output pins for Motor Control
 	set(DDRB,LMOTOR_D);
 	set(DDRB,RMOTOR_D);
+	set(DDRB,6);
 	sei();//enable global interrupts
+	m_clockdivide(0);
 }
 
 void fwd(void){
 	set(MOTOR,LMOTOR_D);
-	set(MOTOR,RMOTOR_D);
+	clear(MOTOR,RMOTOR_D);
 }
 
 void rev(void){
@@ -80,11 +85,12 @@ void stop(void){
 }
 
 ISR(TIMER1_COMPA_vect){    //PWM signal goes low 
+	m_red(TOGGLE);
 	clear(MOTOR,MOTOR_EN);
+	clear(MOTOR,6);
 }
 
 ISR(TIMER1_COMPB_vect){   //PWM signal goes high
 	set(MOTOR,MOTOR_EN);
+	set(MOTOR,6);
 }
-
-
