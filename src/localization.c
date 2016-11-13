c9d3a47237c9f3a4084ff7543310223625d7b989
Becky <14rabramowitz@castilleja.org>
2016-11-12 19:06:12 -0500

    localization in c

--- src/localization.c
+++ src/localization.c
@@ -1,150 +1,186 @@
 #include <stdio.h>
 #include <math.h>
+#include "m_wii.h"
 
-#ifndef max
-    #define max(a,b) ((a) > (b) ? (a) : (b))
-#endif
+int posX, posY;
+double theta;
+unsigned int blobs[10];
 
-#ifndef min
-    #define min(a,b) ((a) < (b) ? (a) : (b))
-#endif
-
-struct Position { int x; int y; };
-
-struct Position center, bot, B, D;
-double orientation;
-
-void set4Pts(int X1, int Y1, int X2, int Y2, int X3, int Y3, int X4, int Y4);
-void set3Pts(int X1, int Y1, int X2, int Y2, int X3, int Y3);
+void set4Pts(unsigned int x[], unsigned int y[]);
+void set3Pts(unsigned int x[], unsigned int y[]);
 double findOrientation();
 void setPosition();
 
-int main() {
-	set4Pts(261, 208, 184, 261, 270, 284, 247, 202);
-	//set4Pts(259, 208, 183, 259, 274, 288, 251, 204);
-
-	//set4Pts(492, 330, 473, 368, 567, 367, 513, 402);
-	//set3Pts(473, 368, 567, 367, 513, 402); // CASE C
-	//set3Pts(492, 330, 473, 368, 567, 367); // CASE A
-	printf("x: %d, y: %d, theta: %f\n", bot.x, bot.y, orientation);
+/* Reads the Wii. Begins execution of localization */
+char readWii() {
+	char recievedWii;
+	//recievedWii = m_wii_read(blobs);
+	if (recievedWii) { 
+		readStars(); 
+		// TODO: Something to send out position of stars
+	}
+	return recievedWii;
 }
 
+/* Reads the stars from the blobs array. 
+   Determines number of stars seen and executes appropriate function. */
+void readStars() {
+	int valCount;
+	unsigned int x_vals[4];
+	unsigned int y_vals[4];
+	int i;
+	for (i = 0; i < 4; i++) {
+		if (blobs[i * 3] != 1023 && blobs[i * 3 + 1] != 1023) {
+			x_vals[valCount] = blobs[i * 3] - 512;
+			y_vals[valCount] = blobs[i * 3 + 1] - 384;
+			valCount++;
+		}
+	}
+	if (valCount == 0) { set4Pts(x_vals, y_vals); }
+	else if (valCount == 1) { set3Pts(x_vals, y_vals); }
+	else if (valCount == 2) { m_usb_tx_string('Sensed 2 stars'); }
+	else if (valCount == 3) { m_usb_tx_string('Sensed 1 star'); }
+	else { m_usb_tx_string('Sensed no stars'); }
+}
 
-// Sets the center of the field in the local frame of the robot
-// Defines the robot's distance (unscaled) from the center of the frame
-void set4Pts(int X1, int Y1, int X2, int Y2, int X3, int Y3, int X4, int Y4) {
+/* Sets the center of the field in the local frame of the robot
+   Defines the robot's distance (unscaled) from the center of the frame */
+void set4Pts(unsigned int x[], unsigned int y[]) {
 	long minDist, maxDist, d12, d13, d14, d23, d24, d34;
-	d12 = pow((X2 - X1), 2) + pow((Y2 - Y1), 2);
-	d13 = pow((X3 - X1), 2) + pow((Y3 - Y1), 2);
-	d14 = pow((X4 - X1), 2) + pow((Y4 - Y1), 2);
-	d23 = pow((X3 - X2), 2) + pow((Y3 - Y2), 2);
-	d24 = pow((X4 - X2), 2) + pow((Y4 - Y2), 2);
-	d34 = pow((X4 - X3), 2) + pow((Y4 - Y3), 2);
-	maxDist = max (max (max(d12, d13), max(d14, d23)), max(d24, d34));
-	minDist = min (min (min(d12, d13), min(d14, d23)), min(d24, d34));
-
-	// check largest distance values to determine B and D, find center
-	if (maxDist == d12) { 
-		if (minDist == d13 || minDist == d14) {
-			B.x = X1; B.y = Y1; D.x = X2; D.y = Y2;
-		} else { D.x = X1; D.y = Y1; B.x = X2; B.y = Y2; }
-	} else if (maxDist == d13) { 
-		if (minDist == d12 || minDist == d14) {
-			B.x = X1; B.y = Y1; D.x = X3; D.y = Y3;
-		} else { D.x = X1; D.y = Y1; B.x = X3; B.y = Y3; }
-	} else if (maxDist == d14) {
-		if (minDist == d13 || minDist == d12) {
-			B.x = X1; B.y = Y1; D.x = X4; D.y = Y4;
-		} else { D.x = X1; D.y = Y1; B.x = X4; B.y = Y4; }
-	} else if (maxDist == d23) {
-		if (minDist == d12 || minDist == d24) {
-			B.x = X2; B.y = Y2; D.x = X3; D.y = Y3;
-		} else { D.x = X2; D.y = Y2; B.x = X3; B.y = Y3; }
-	} else if (maxDist == d24) {
-		if (minDist == d12 || minDist == d23) {
-			B.x = X2; B.y = Y2; D.x = X4; D.y = Y4;
-		} else { D.x = X2; D.y = Y2; B.x = X4; B.y = Y4; } 
-	} else if (maxDist == d34) {
-		if (minDist == d13 || minDist == d23) {
-			B.x = X3; B.y = Y3; D.x = X4; D.y = Y4;
-		} else { D.x = X3; D.y = Y3; B.x = X4; B.y = Y4; }
-	} else { 
-		printf("Something is wrong. Largest distance is %ld\n", maxDist);
-	}
-	printf("d12: %ld, d13: %ld, d14: %ld, d23: %ld, d24: %ld, d34: %ld\n", d12, d13, d14, d23, d24, d34);
-	printf("minDist: %ld, maxDist: %ld, Bx: %d, By: %d, Dx: %d, Dy: %d\n", minDist, maxDist, B.x, B.y, D.x, D.y);
-	center.x = (B.x + D.x) / 2;
-	center.y = (B.y + D.y) / 2;
-	orientation = findOrientation(B, D);
-	setPosition();
+	char B, D;
+	unsigned int centerx, centery;
+	d12 = pow((x[2] - x[1]), 2) + pow((y[2] - y[1]), 2);
+	d13 = pow((x[3] - x[1]), 2) + pow((y[3] - y[1]), 2);
+	d14 = pow((x[4] - x[1]), 2) + pow((y[4] - y[1]), 2);
+	d23 = pow((x[3] - x[2]), 2) + pow((y[3] - y[2]), 2);
+	d24 = pow((x[4] - x[2]), 2) + pow((y[4] - y[2]), 2);
+	d34 = pow((x[4] - x[3]), 2) + pow((y[4] - y[3]), 2);
+	// calculate maximum distance between blobs
+	maxDist =  d12     > d13 ?  d12     : d13;
+	maxDist =  maxDist > d14 ?  maxDist : d14;
+	maxDist =  maxDist > d23 ?  maxDist : d23;
+	maxDist =  maxDist > d24 ?  maxDist : d24;
+	maxDist =  maxDist > d34 ?  maxDist : d34;
+	// calcualte minimum distance between blobs
+	minDist =  d12     < d13 ?  d12     : d13;
+	minDist =  minDist < d14 ?  minDist : d14;
+	minDist =  minDist < d23 ?  minDist : d23;
+	minDist =  minDist < d24 ?  minDist : d24;
+	minDist =  minDist < d34 ?  minDist : d34;
+	// compare distance values to find B & D
+	if (maxDist == d12) {
+    	if (minDist == d13 || minDist == d14) { B = 1; D = 2; } 
+    	else { B = 2; D = 1; }
+	} else if (maxDist == d13) {
+		if (minDist == d12 || minDist == d14) { B = 1; D = 3; } 
+		else { B = 3; D = 1; }
+    } else if (maxDist == d14) {
+    	if (minDist == d13 || minDist == d12) { B = 1; D = 4; }
+    	else { B = 4; D = 1; }
+    } else if (maxDist == d23) {
+    	if (minDist == d12 || minDist == d24) { B = 2; D = 3; }
+    	else { B = 3; D = 2; }
+    } else if (maxDist == d24) {
+    	if (minDist == d12 || minDist == d23) { B = 2; D = 4; }
+    	else { B = 4; D = 2; }
+    } else if (maxDist == d34) {
+    	if (minDist == d13 || minDist == d23) { B = 3; D = 4; }
+    	else { B = 4; D = 3;}
+    }
+    centerx = (x[B] + x[D]) / 2;
+	centery = (y[B] + y[D]) / 2;
+	theta = findOrientation(x[B], y[B], x[D], y[D]);
+	setPosition(centerx, centery);
 }
 
-void set3Pts(int X1, int Y1, int X2, int Y2, int X3, int Y3) {
+void set3Pts(unsigned int x[], unsigned int y[]) {
 	long minDist, maxDist, d12, d13, d23;
-	d12 = pow((X2 - X1), 2) + pow((Y2 - Y1), 2);
-	d13 = pow((X3 - X1), 2) + pow((Y3 - Y1), 2);
-	d23 = pow((X3 - X2), 2) + pow((Y3 - Y2), 2);
-	maxDist = max (max(d12, d13), d23);
-	minDist = min (min(d12, d13), d23);
+	char A, B, C, D;
+	unsigned int centerx, centery;
+	d12 = pow((x[2] - x[1]), 2) + pow((y[2] - y[1]), 2);
+	d13 = pow((x[3] - x[1]), 2) + pow((y[3] - y[1]), 2);
+	d23 = pow((x[3] - x[2]), 2) + pow((y[3] - y[2]), 2);
+	// calculate maximum distance between blobs
+	maxDist =  d12     > d13 ?  d12     : d13;
+	maxDist =  maxDist > d23 ?  maxDist : d23;
+	// calcualte minimum distance between blobs
+	minDist =  d12     < d13 ?  d12     : d13;
+	minDist =  minDist < d23 ?  minDist : d23;
 	double ratio = (double) maxDist / minDist;
-	if (ratio < 2.0) {
-		// Case in which B is missing: Max is CD, Min is AD
-
-	} else if (ratio > 4.0) {
-		// Case in which A is missing: Max is BD, Min is BC
+	if (ratio < 2.0) { // B missing, Max: CD, Min: AD
 		if (maxDist == d12) {
-			if (minDist == d13) {
-				B.x = X1; B.y = Y1; D.x = X2; D.y = Y2; 
-			} else { D.x = X1; D.y = Y1; B.x = X2; B.y = Y2; }
+			if (minDist == d13) { A = 3; C = 2; D = 1; }
+			else { A = 3; C = 1; D = 2; }
 		} else if (maxDist == d13) {
-			if (minDist == d12) {
-				B.x = X1; B.y = Y1; D.x = X3; D.y = Y3;
-			} else { D.x = X1; D.y = Y1; B.x = X3; B.y = Y3; }
+			if (minDist == d12) { A = 2; C = 3; D = 1; }
+			else { A = 2; C = 1; D = 3; }
 		} else if (maxDist == d23) {
-			if (minDist == d13) {
-				B.x = X3; B.y = Y3; D.x = X2; D.y = Y2; 
-			} else { D.x = X3; D.y = Y3; B.x = X2; B.y = Y2; }
+			if (minDist == d12) { A = 1; C = 3; D = 2; }
+			else { A = 1; C = 2; D = 3; }
 		}
-		center.x = (B.x + D.x) / 2; 
-		center.y = (B.y + D.y) / 2;
-		orientation = findOrientation(B, D);
-		setPosition();
-	} else if (ratio < 3.2) {
-		// Case in which D is missing: Max is AC, Min is BC
-
-	} else {
-		// Case in which C is missing: Max is BD, Min is AC
+		int Mx, My;
+		Mx = x[C] + (x[C] - x[A]) * 0.5245;
+		My = y[C] + (y[C] - y[A]) * 0.5245;
+		centerx = Mx - (x[D] + (x[D] - Mx) * 0.273);
+		centery = My - (y[D] + (y[D] - My) * 0.273);
+		theta = findOrientation(centerx, centerx, x[D], y[D]);
+	} else if (ratio > 4.0) { // A missing, Max: BD, Min: BC
 		if (maxDist == d12) {
-			if (minDist == d13) {
-				B.x = X1; B.y = Y1; D.x = X2; D.y = Y2; 
-			} else { D.x = X1; D.y = Y1; B.x = X2; B.y = Y2; }
+			if (minDist == d13) { B = 1; D = 2; }
+			else { B = 2; D = 1; }
 		} else if (maxDist == d13) {
-			if (minDist == d12) {
-				B.x = X1; B.y = Y1; D.x = X3; D.y = Y3; 
-			} else { D.x = X1; D.y = Y1; B.x = X3; B.y = Y3; }
+			if (minDist == d12) { B = 1; D = 3; }
+			else { B = 3; D = 1; }
 		} else if (maxDist == d23) {
-			if (minDist == d13) {
-				B.x = X3; B.y = Y3; D.x = X2; D.y = Y2; 
-			} else { D.x = X3; D.y = Y3; B.x = X2; B.y = Y2; }
+			if (minDist == d13) { B = 3; D = 2; }
+			else { B = 2; D = 3; }
 		}
-		center.x = (B.x + D.x) / 2; 
-		center.y = (B.y + D.y) / 2;
-		orientation = findOrientation(B, D);
-		setPosition();
+		centerx = (x[B] + x[D]) / 2;
+		centery = (y[B] + y[D]) / 2;
+		theta = findOrientation(x[B], y[B], x[D], y[D]);
+	} else if (ratio < 3.2) { // D missing, Max: AC, Min: BC
+		if (maxDist == d12) {
+			if (minDist == d13) { A = 2; B = 3; C = 1; }
+			else { A = 1; B = 3; C = 2; }
+		} else if (maxDist == d13) {
+			if (minDist == d12) { A = 3; B = 2; C = 1; }
+			else { A = 1; B = 2; C = 3; }
+		} else if (maxDist == d23) {
+			if (minDist == d12) { A = 3; B = 1; C = 2; }
+			else { A = 2; B = 1; C = 3; }
+		}
+		int Mx, My;
+		Mx = x[C] + (x[C] - x[A]) * 0.5245;
+		My = y[C] + (y[C] - y[A]) * 0.5245;
+		centerx = Mx - (x[B] + (x[B] - Mx) * 1.604);
+		centery = My - (y[B] + (y[B] - My) * 1.604);
+		theta = findOrientation(centerx, centerx, x[D], y[D]);
+	} else { // C missing, Max: BD, Min: AC
+		if (maxDist == d12) { 
+			if (minDist == d13) { B = 1; D = 2; }
+			else { B = 2; D = 1; }
+		} else if (maxDist == d13) {
+			if (minDist == d12) { B = 1; D = 3; }
+			else { B = 3; D = 1; }
+		} else if (maxDist == d23) {
+			if (minDist == d13) { B = 3; D = 2; }
+			else { B = 2; D = 3; }
+		}
+		centerx = (x[B] + x[D]) / 2;
+		centery = (y[B] + y[D]) / 2;
+		theta = findOrientation(x[B], y[B], x[D], y[D]);
 	}
+	setPosition(centerx, centery);
 }
 
 
-/* Determines orientation of the puck given two input positions. In ideal case, 
-* values should be inputted in order (B, D) but one can be substituted with
-* the center position if either B or D not given */
-double findOrientation(struct Position p1, struct Position p2) {
-	//printf("Bx: %d, By: %d, Dx: %d, Dy: %d\n", B.x, B.y, D.x, D.y);
-	return atan2((p1.x - p2.x),(p1.y - p2.y));
+/* Determines orientation of the puck given two input positions. */
+double findOrientation(int Bx, int By, int Dx, int Dy) {
+	return atan2((Bx - Dx), (By - Dy));
 }
 
 /* Sets position of the robot in coordinates relative to the global frame */
-void setPosition() {
-	bot.x = (center.x * cos(orientation) - center.y * sin(orientation));
-	bot.y = (center.x * sin(orientation) + center.y * cos(orientation));
+void setPosition(int centerx, int centery) {
+	posX = -(centerx * cos(theta) - centery * sin(theta));
+	posY = (centerx * sin(theta) + centery * cos(theta));
 }
